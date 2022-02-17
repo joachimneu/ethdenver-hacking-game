2e5d959808e4b6c978f8080a6cb3088b434c44fc
Lei Yang <lei@yangl1996.com>
2022-02-17 16:37:05 -0700

    add spaceship

--- game/src/Spaceship.sol
+++ game/src/Spaceship.sol
@@ -1,11 +1,30 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.10;
 
+import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
-import "../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract Spaceship is ERC20, ERC20Burnable {
+contract Spaceship is ERC20, Ownable {
+    address planet;
     constructor() ERC20("Spaceship", "SHIP") {
-        
+    }
+
+    function setup(address planetAddr) public onlyOwner {
+        planet = planetAddr;
+    }
+
+    function mine(address recipient, uint256 amount) public {
+        assert(msg.sender == planet);
+        _mint(recipient, amount);
+    }
+
+    function tryBurn(address account, uint256 amount) public returns (bool ok) {
+        assert(msg.sender == planet);
+        if (balanceOf(account) >= amount) {
+            _burn(account, amount);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
