a99a592f5b890bbe2a56b52b6408b4b14978b328
Lei Yang <lei@yangl1996.com>
2022-02-17 15:55:07 -0700

    mineable and burnable uranium

--- game/src/Uranium.sol
+++ game/src/Uranium.sol
@@ -2,10 +2,29 @@
 pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20FlashMint.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
 
-contract Uranium is ERC20, ERC20Burnable {
+contract Uranium is ERC20, Ownable {
+    address planet;
     constructor() ERC20("Uranium", "URAN") {
-        
+    }
+
+    function setup(address planetAddr) public onlyOwner {
+        planet = planetAddr;
+    }
+
+    function mine(address recipient, uint256 amount) public {
+        assert(msg.sender == planet);
+        _mint(recipient, amount);
+    }
+
+    function tryBurn(address account, uint256 amount) public returns (bool ok) {
+        assert(msg.sender == planet);
+        if (balanceOf(account) >= amount) {
+            _burn(account, amount);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
