1e59278ae4d40201bd228a603940a0c3605e606c
Lei Yang <lei@yangl1996.com>
2022-02-17 23:34:35 -0700

    mint a token in test

--- game/src/test/Galaxy.t.sol
+++ game/src/test/Galaxy.t.sol
@@ -5,10 +5,17 @@ import "ds-test/test.sol";
 import "../Resource.sol";
 import "../Galaxy.sol";
 
+interface CheatCodes {
+    function roll(uint256) external;
+}
+
 contract ContractTest is DSTest {
     Galaxy g;
     Resource u;
     Resource s;
+    uint256 tokenId;
+
+    CheatCodes cheats = CheatCodes(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
 
     function setUp() public {
         u = new Resource("Uranium", "U");
@@ -16,5 +23,9 @@ contract ContractTest is DSTest {
         g = new Galaxy(address(u), address(s));
         u.setupGalaxy(address(g));
         s.setupGalaxy(address(g));
+        cheats.roll(10);
+        tokenId = g.discoveryBegin();
+        cheats.roll(20);
+        g.discoveryFinalize(tokenId);
     }
 }
